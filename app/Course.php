2810d431c94ec682289075cb831ffeb2a5105ddc
IronySoft <ironyoft.ltd@gmail.com>
2019-08-15 12:12:38 +0600

    Course Table Created

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable=[
+        'department_id','course_name'
+    ];
+
+
+    function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
